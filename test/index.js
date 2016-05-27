e6a177c6305394ca2924efbaeedce56ce10cec25
AzSiAz <slider.perso@live.fr>
2016-05-27 11:09:10 +0200

    remove test update

--- test/index.js
+++ test/index.js
@@ -7,14 +7,14 @@ describe('Test littlerequester', function () {
     describe('Test HTTP & HTTPS', function () {
         it("should support http requests", function (done) {
             this.timeout(10000);
-            littlerequester({url: "http://example.com/", type: "raw"}).then((data) => {
+            littlerequester({url: "http://example.com/", type: "raw"}).then(function(data) {
                 assert.equal(!!/this domain/gi.test(data), true);
                 done();
             });
         });
         it("should support https requests", function (done) {
             this.timeout(10000);
-            littlerequester({url: "https://github.com", type: "raw"}).then((data) => {
+            littlerequester({url: "https://github.com", type: "raw"}).then(function(data) {
                 assert.equal(!!/GitHub/gi.test(data), true);
                 done();
             });
@@ -23,7 +23,7 @@ describe('Test littlerequester', function () {
     describe('GET Method', function () {
         it('Should return JSON', function (done) {
             this.timeout(10000);
-            littlerequester({url: "https://api.azsiaz.tech/ln/english", type: "json"}).then((data) => {
+            littlerequester({url: "https://api.azsiaz.tech/ln/english", type: "json"}).then(function(data) {
                 assert.equal(typeof data, "object");
                 assert.equal(data.type, "Light_novel");
                 done();
@@ -31,7 +31,7 @@ describe('Test littlerequester', function () {
         });
         it('Should return Raw Data', function (done) {
             this.timeout(10000);
-            littlerequester({url: "http://monip.org/", type: "raw"}).then((data) => {
+            littlerequester({url: "http://monip.org/", type: "raw"}).then(function(data) {
                 assert.equal(typeof data, "string");
                 assert.equal(!!/IP/gi.test(data), true);
                 done();
