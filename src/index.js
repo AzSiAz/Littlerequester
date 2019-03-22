2be87b7bb7902b0d13384a7c9dda728361524d32
AzSiAz <slider.perso@live.fr>
2019-03-22 12:44:41 +0100

    new code

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const http = require('http')
+const https = require('https')
+
+const { parseOptions } = require('./parseObject')
+
+
+/**
+ * @param {string | import("./parseobject").Raw} options
+ * @param {string} [typeData="raw"]
+ */
+module.exports = (options, typeData = 'raw') => new Promise((resolve, reject) => {
+    const opts = parseOptions(options, typeData)
+    const requester = opts.request.protocol.includes('http:') ? http : https
+
+    const res = requester.request(opts.request, response => {
+        response.setEncoding("utf-8")
+        let str = ''
+
+        response
+            .on('data', chunk => str += chunk)
+            .on('error', err => reject(err))
+            .on('end', () => {
+                if (opts.type.toLowerCase() === 'raw') return resolve({ data: str, res: response })
+
+                console.log(str)
+
+                const json = JSON.parse(str)
+
+                resolve({ data: json, res: response })
+            })
+    })
+
+    res.on('error', err => reject(err))
+
+    if (opts.request.method !== 'GET' && opts.data) res.write(opts.data)
+
+    res.end()
+})
